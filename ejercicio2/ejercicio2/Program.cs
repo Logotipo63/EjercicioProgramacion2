2b2a4c7c82abd77cff25edc0c3f492d2a10db3de
carlostaborda1987-boop <carlostaborda1987@gmail.com>
2026-09-16 10:06:58 +0200

    ejercicio3

--- ejercicio2/ejercicio2/Program.cs
+++ ejercicio2/ejercicio2/Program.cs
@@ -6,10 +6,14 @@
 
 //Ejercicio 2
 
-Console.WriteLine("Esribe un numero entero");
+/* Console.WriteLine("Esribe un numero entero");
 int numero2 =int.Parse(Console.ReadLine());
 if (numero2 > 0) Console.WriteLine("El numero es mayor que cero");
 else Console.WriteLine("El numero no es mayor que cero");
    
-
-
+ */
+ // ejercicio3
+ Console.WriteLine("pon tu nota");
+ double nota3 =double.Parse(Console.ReadLine());
+ if (nota3>=5) Console.WriteLine("aprobado");
+ 
