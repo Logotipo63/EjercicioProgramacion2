d53c3a534f02dc88a4d57552f60f7f808f3cec58
Humberto <hgomezserrano@gmail.com>
2026-09-16 09:30:02 +0200

    Segundo commit

--- ejercicio2/ejercicio2/Program.cs
+++ ejercicio2/ejercicio2/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System.Diagnostics.Tracing;
 
-Console.WriteLine("cual es tu edad?");
-int edad =int.Parse(Console.ReadLine());
-if (edad>=18) Console.WriteLine("es mayor de edad");
+//Console.WriteLine("cual es tu edad?");
+//int edad =int.Parse(Console.ReadLine());
+//if (edad>=18) Console.WriteLine("es mayor de edad");
+
+//Ejercicio 2
+
+Console.WriteLine("Esribe un numero entero");
+int numero2 =int.Parse(Console.ReadLine());
+if (numero2 > 0) Console.WriteLine("El numero es mayor que cero");
+else Console.WriteLine("El numero no es mayor que cero");
+   
 
 
